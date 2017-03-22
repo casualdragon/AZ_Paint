db34aa9d9ed2cbd29f57c433b63ca2f0588071b6
taylor.zachary11@gmail.com <lDdrLdO9SxWzMfA01>
2017-03-22 09:49:21 -0500

    Test Commit

--- app/src/main/java/edu/apsu/csci/teamaz/azpaint/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/teamaz/azpaint/MainActivity.java
@@ -3,6 +3,8 @@ package edu.apsu.csci.teamaz.azpaint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//This comment is for a test
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
